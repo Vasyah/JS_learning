8fe910f14d8a2629ab138e8614b3b160e27bfeff
Vasyah <Souji_933@mail.ru>
2020-04-08 16:24:20 +0700

    16 урок получение элементов со страницы

---
+++ lesson 016 getElements/script.js
@@ -0,0 +1,9 @@
+var d = document;
+var circles = d.getElementsByTagName('div');
+var circle2 = d.getElementsByClassName('circle');
+
+var square1 = d.getElementById('square');
+console.log(circles);
+console.log(circle2[0]);
+console.log(circle2);
+console.log(square1);
